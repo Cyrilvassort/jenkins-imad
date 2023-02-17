c8b57efba874a630155059e59b47f6c477e35319
cyril vassort <cyril.vassort@cloud-temple.fr>
2023-02-17 14:14:37 +0100

    test cicd

--- backend/uni_test.py
+++ backend/uni_test.py
@@ -1,90 +1,38 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-from pymongo import MongoClient
 import unittest
+from app import *
 
-app = Flask(__name__)
-CORS(app)
-client = MongoClient('mongodb://root:example@mongo:27017/')
-db = client["db"]
-col = db["employees"]
+class TestEmployeeAPI(unittest.TestCase):
 
-
-class EmployeeAPI:
-    def __init__(self):
-        pass
-
-@staticmethod
-@app.route('/api/v1/employees', methods=['GET', 'POST'])
-def employees():
-    if request.method == 'GET':
-        employees_list = []
-        try:
-            cursor = col.find({}, {'_id': 0})
-            for employee in cursor:
-                employees_list.append(employee)
-        except ValueError as err:
-            return jsonify({'message': 'Erreur : {}'.format(str(err))}), 500
-        return jsonify(employees_list), 200
-
-    if request.method == 'POST':
-        data = request.get_json()
-        cursor = col.find({}, {'_id': 0})
-        try:
-            employee_id = max([employee['id'] for employee in cursor]) + 1
-        except ValueError:
-            employee_id = 1
-        data['id'] = employee_id
-        col.insert_one(data)
-        return jsonify(data), 200
-
-
-
-    @staticmethod
-    @app.route('/api/v1/employees/<int:employee_id>', methods=['GET', 'DELETE', 'PUT'])
-    def employee(employee_id):
-        if request.method == 'GET':
-            employee = col.find_one({'id': employee_id}, {'_id': 0})
-            if employee:
-                return jsonify(employee), 200
-            else:
-                return jsonify({'message': 'Employee not found'}), 404
-
-        if request.method == 'DELETE':
-            result = col.delete_one({'id': employee_id})
-            if result.deleted_count == 1:
-                return jsonify({'message': 'Employee deleted successfully'}), 200
-            else:
-                return jsonify({'message': 'Employee not found'}), 404
-
-        if request.method == 'PUT':
-            data = request.get_json()
-            result = col.update_one({'id': employee_id}, {'$set': data})
-            if result.modified_count == 1:
-                return jsonify({'message': 'Employee updated successfully'}), 200
-            else:
-                return jsonify({'message': 'Employee not found'}), 404
-
-class EmployeeAPITestCase(unittest.TestCase):
-    def setUp(self):
-        self.app = app.test_client()
-        self.apihandler = EmployeeAPI()
-    
-    def test_get_method(self):
-        response = self.app.get('/api/v1/employees')
+    # Test the GET request
+    def test_get(self):
+        response = employees()
         self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(response.json())
 
-    def test_post_method(self):
-        response = self.app.post('/api/v1/employees', data= "{'name': 'ABC', 'address': '123 main st'}")
+    # Test the POST request
+    def test_post(self):
+        data = {
+            'firstName': 'John Doe',
+            'lastName': 'engineer',
+            'emailId': 'hellopodjezpinfpzeoinoinfezoijnoi'
+        }
+        response = employees(method="POST", json=data)
         self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(response.json()['id'])
 
-    def test_put_method(self):
-        self.app.post('/api/v1/employees', data= "{'name': 'ABC', 'address': '123 main st'}")
-        response = self.app.put('/api/v1/employees/1', data="{'name': 'XYZ', 'address': '456 main st'}")
+    # Test the PUT request
+    def test_put(self):
+        data = {
+            'firstName': 'Jane Doe',
+            'lastName': 'administrator',
+            'emailId': 'hello@gmail.com'
+        }
+        response = employees(employee_id=1, method="PUT", json=data)
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['message'], 'Employee updated successfully')
 
-
-    def test_delete_method(self):
-        self.app.post('/api/v1/employees', data = "{'name': 'ABC', 'address': '123 main st'}")
-        response = self.app.delete('/api/v1/employees/1')
-        self.assertEqual(response.status_code, 200) 
+    # Test the DELETE request
+    def test_delete(self):
+        response = employees(employee_id=1, method="DELETE")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['message'], 'Employee deleted successfully')
