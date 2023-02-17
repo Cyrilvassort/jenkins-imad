ba59323be6531f812c6b3d206f477d524cb7a469
cyril vassort <cyril.vassort@cloud-temple.fr>
2023-02-17 14:07:40 +0100

    test cicd

--- backend/uni_test.py
+++ backend/uni_test.py
@@ -1,42 +1,90 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from pymongo import MongoClient
 import unittest
-import requests
 
-class TestEmployeeAPI(unittest.TestCase):
-    
-    base_url = "http://localhost:8080/api/v1"
-    
-    def test_get_employees(self):
-        response = requests.get(f"{self.base_url}/employees")
-        self.assertEqual(response.status_code, 200)
-        self.assertIsInstance(response.json(), list)
-    
-    def test_add_employee(self):
-        new_employee = {
-            "firstName": "Jean",
-            "lastName": "Lasalle",
-            "emailId": "jean.lasalle@laposte.net"
-        }
-        response = requests.post(f"{self.base_url}/employees", json=new_employee)
-        self.assertEqual(response.status_code, 200)
-        self.assertIsInstance(response.json(), dict)
-    
-    def test_update_employee(self):
-        updated_employee = {
-            "firstName": "Jeanne",
-            "lastName": "Lasalle",
-            "emailId" : "jeanne.lasalle@laposte.net"
-        }
-        employee_id = 1
-        response = requests.put(f"{self.base_url}/employees/{employee_id}", json=updated_employee)
-        self.assertEqual(response.status_code, 204)
-    
-    def test_delete_employee(self):
-        employee_id = 1
-        response = requests.delete(f"{self.base_url}/employees/{employee_id}")
-        self.assertEqual(response.status_code, 204)
+app = Flask(__name__)
+CORS(app)
+client = MongoClient('mongodb://root:example@mongo:27017/')
+db = client["db"]
+col = db["employees"]
 
-if __name__ == "__main__":
-    test_order = ["test_get_employees", "test_add_employee", "test_update_employee", "test_delete_employee"] # important so the delete test will work
-    loader = unittest.TestLoader()
-    loader.sortTestMethodsUsing = lambda x, y: test_order.index(x) - test_order.index(y)
-    unittest.main(testLoader=loader, verbosity=2)
+
+class EmployeeAPI:
+    def __init__(self):
+        pass
+
+@staticmethod
+@app.route('/api/v1/employees', methods=['GET', 'POST'])
+def employees():
+    if request.method == 'GET':
+        employees_list = []
+        try:
+            cursor = col.find({}, {'_id': 0})
+            for employee in cursor:
+                employees_list.append(employee)
+        except ValueError as err:
+            return jsonify({'message': 'Erreur : {}'.format(str(err))}), 500
+        return jsonify(employees_list), 200
+
+    if request.method == 'POST':
+        data = request.get_json()
+        cursor = col.find({}, {'_id': 0})
+        try:
+            employee_id = max([employee['id'] for employee in cursor]) + 1
+        except ValueError:
+            employee_id = 1
+        data['id'] = employee_id
+        col.insert_one(data)
+        return jsonify(data), 200
+
+
+
+    @staticmethod
+    @app.route('/api/v1/employees/<int:employee_id>', methods=['GET', 'DELETE', 'PUT'])
+    def employee(employee_id):
+        if request.method == 'GET':
+            employee = col.find_one({'id': employee_id}, {'_id': 0})
+            if employee:
+                return jsonify(employee), 200
+            else:
+                return jsonify({'message': 'Employee not found'}), 404
+
+        if request.method == 'DELETE':
+            result = col.delete_one({'id': employee_id})
+            if result.deleted_count == 1:
+                return jsonify({'message': 'Employee deleted successfully'}), 200
+            else:
+                return jsonify({'message': 'Employee not found'}), 404
+
+        if request.method == 'PUT':
+            data = request.get_json()
+            result = col.update_one({'id': employee_id}, {'$set': data})
+            if result.modified_count == 1:
+                return jsonify({'message': 'Employee updated successfully'}), 200
+            else:
+                return jsonify({'message': 'Employee not found'}), 404
+
+class EmployeeAPITestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        self.apihandler = EmployeeAPI()
+    
+    def test_get_method(self):
+        response = self.app.get('/api/v1/employees')
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_method(self):
+        response = self.app.post('/api/v1/employees', data= "{'name': 'ABC', 'address': '123 main st'}")
+        self.assertEqual(response.status_code, 200)
+
+    def test_put_method(self):
+        self.app.post('/api/v1/employees', data= "{'name': 'ABC', 'address': '123 main st'}")
+        response = self.app.put('/api/v1/employees/1', data="{'name': 'XYZ', 'address': '456 main st'}")
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_delete_method(self):
+        self.app.post('/api/v1/employees', data = "{'name': 'ABC', 'address': '123 main st'}")
+        response = self.app.delete('/api/v1/employees/1')
+        self.assertEqual(response.status_code, 200) 
